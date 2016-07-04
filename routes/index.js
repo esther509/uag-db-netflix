ee1d44113935f2b93bb39f67828deafbab811e12
Laura Esther Lopez <esther_509@hotmail.com>
2016-07-03 19:12:58 -0500

    :sparkles: SE optimizaron las dependencias, tambien se borraron archivos que no se necesitan.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 ﻿var express = require('express');
+var http = require('http');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+    res.render('index', { title: 'UAG Netflix' }); 
 });
 
 module.exports = router;
