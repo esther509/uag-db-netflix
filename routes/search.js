d11d98d61a04383a65d092c6c6220d7e5c1c8fd1
Sergio de Jesus Sanchez Robles <sergioj.sanchezr@gmail.com>
2016-08-14 18:31:26 -0500

    Search implemented

--- routes/search.js
+++ routes/search.js
@@ -11,7 +11,104 @@ var connectionString = process.env.DATABASE_URL || "postgres://Sergio:admin@loca
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('pages/search', helper.createRenderParams(req.session));
+    console.log(req.query);
+
+    var q = req.query.q = req.query.q || "";
+    var t = req.query.t = req.query.t || "";
+    var d = req.query.d = req.query.d || "";
+    var c = req.query.c = req.query.c || "";
+    var y = req.query.y = req.query.y || "";
+    var g = req.query.g = req.query.g || "";
+
+    var reqQuery = req.query;
+
+    var having = "";
+    if (q != "") {
+        having = "HAVING movie.name ILIKE '%" + q +
+            "%' OR string_agg(DISTINCT(director.name), ', ') ILIKE '%" + q +
+            "%' OR string_agg(DISTINCT(category.name), ', ') ILIKE '%" + q + 
+            "%' OR releasedate ILIKE '%" + q + 
+            "%' OR country ILIKE '%" + q + 
+            "%' OR awards ILIKE '%" + q + "%'";
+    } else {
+        options = [];
+        if (t != "") {
+            options.push("movie.name ILIKE '%" + t+ "%'");
+        }
+        if (d != "") {
+            options.push("string_agg(DISTINCT(director.name), ', ') ILIKE '%" + d + "%'");
+        }
+        if (g != "") {
+            options.push("string_agg(DISTINCT(category.name), ', ') ILIKE '%" + g + "%'");
+        }
+        if (c != "") {
+            options.push("country ILIKE '%" + c + "%'");
+        }
+        if (y != "") {
+            options.push("releasedate ILIKE '%" + y + "%'");
+        }
+
+        if (options.length > 0 ) 
+            having = 'HAVING ' + options.join(' AND ');
+    }
+
+    console.log(having);
+
+    if (having == "") {
+        res.render('pages/search', helper.createRenderParams(req.session, { query: reqQuery, results: [] }));
+        return;
+    }
+
+    pg.connect(connectionString, function (err, client, done) {
+
+        var queryString = `
+        SELECT
+            movie.id as movieid,
+            movie.name as moviename,
+            country,
+            year,
+            posterurl,
+            videourl,
+            releasedate,
+            plot,
+            awards,
+            metascore,
+            string_agg(DISTINCT(director.name), ', ') as directors,
+            string_agg(DISTINCT(category.name), ', ') as categories,
+            string_agg(DISTINCT(acts_in.actor_name), ', ') as actors
+        FROM director 
+        INNER JOIN directed_by ON director.id = directed_by.director_id
+        INNER JOIN movie ON movie.id = directed_by.movie_id
+        INNER JOIN acts_in ON movie.id = acts_in.movie_id
+        INNER JOIN movie_category ON movie.id = movie_category.movie_id
+        INNER JOIN category ON category.id = movie_category.category_id 
+        GROUP BY movie.id ` + having;
+
+        var query = client.query(queryString);
+
+        query.on('error', function (err) {
+            done();
+            console.log(err);
+            res.render('pages/search', helper.createRenderParams(req.session, { query: reqQuery, results: [] }));
+        });
+
+        query.on('row', function (row, result) {
+            result.addRow(row);
+        });
+
+        var sess = req.session;
+        query.on('end', function (result) {
+
+            done();
+            console.log(result);
+            for (i = 0; i < result.rows; i++) {
+                console.log(result.rows[0]);
+            }
+
+            res.render('pages/search', helper.createRenderParams(req.session, { query: reqQuery, results: result.rows }));
+        });
+    });
+
 });
 
 module.exports = router;
--- routes/single.js
+++ routes/single.js
@@ -19,13 +19,39 @@ router.get('/:movieid', function (req, res) {
 
     pg.connect(connectionString, function (err, client, done) {
 
-        var query = client.query("SELECT movie.id as movieid, movie.name as moviename, country, year, posterurl, videourl, releasedate, plot, director.name as directorname, awards, metascore, " +
-            " (SELECT string_agg(actor_name, ', ') from acts_in INNER JOIN movie ON id= movie_id WHERE movie.id = $1) as actors, " +
-            " (SELECT string_agg(category.name, ', ') from movie_category, category, movie WHERE movie.id = $1 AND movie.id = movie_id AND category.id = category_id) as categories " +
-            " FROM director INNER JOIN " +
-            " (movie INNER JOIN directed_by ON movie.id = directed_by.movie_id) " +
-            " ON director.id = directed_by.director_id " +
-            " WHERE public.movie.id = $1", [movieid]);
+        var queryString = `
+        SELECT
+            movie.id as movieid,
+            movie.name as moviename,
+            country,
+            year,
+            posterurl,
+            videourl,
+            releasedate,
+            plot,
+            awards,
+            metascore,
+            string_agg(DISTINCT(director.name), ', ') as directors,
+            string_agg(DISTINCT(category.name), ', ') as categories,
+            string_agg(DISTINCT(acts_in.actor_name), ', ') as actors
+        FROM director 
+        INNER JOIN directed_by ON director.id = directed_by.director_id
+        INNER JOIN movie ON movie.id = directed_by.movie_id
+        INNER JOIN acts_in ON movie.id = acts_in.movie_id
+        INNER JOIN movie_category ON movie.id = movie_category.movie_id
+        INNER JOIN category ON category.id = movie_category.category_id
+        WHERE movie.id = $1 
+        GROUP BY movie.id`;
+
+        //var query = client.query("SELECT movie.id as movieid, movie.name as moviename, country, year, posterurl, videourl, releasedate, plot, director.name as directorname, awards, metascore, " +
+        //    " (SELECT string_agg(actor_name, ', ') from acts_in INNER JOIN movie ON id= movie_id WHERE movie.id = $1) as actors, " +
+        //    " (SELECT string_agg(category.name, ', ') from movie_category, category, movie WHERE movie.id = $1 AND movie.id = movie_id AND category.id = category_id) as categories " +
+        //    " FROM director INNER JOIN " +
+        //    " (movie INNER JOIN directed_by ON movie.id = directed_by.movie_id) " +
+        //    " ON director.id = directed_by.director_id " +
+        //    " WHERE public.movie.id = $1", [movieid]);
+
+        var query = client.query(queryString, [movieid]);
 
         query.on('error', function (err) {
             done();
